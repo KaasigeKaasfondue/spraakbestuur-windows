0440a44e0c33073c720efd10b378e567eb5874b0
KaasigeKaasfondue <155380628+KaasigeKaasfondue@users.noreply.github.com>
2025-06-17 01:23:47 +0200

    Logs

--- main.js
+++ main.js
@@ -8,20 +8,35 @@ function createWindow() {
     width: 800,
     height: 600,
   });
+
   win.loadFile('index.html');
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  autoUpdater.checkForUpdatesAndNotify();
+  if (app.isPackaged) {
+    autoUpdater.checkForUpdatesAndNotify();
 
-  autoUpdater.on('update-available', () => {
-    console.log('Update beschikbaar');
-  });
+    autoUpdater.on('checking-for-update', () => {
+      console.log('Zoekt naar updates...');
+    });
 
-  autoUpdater.on('update-downloaded', () => {
-    console.log('Update gedownload, app wordt herstart');
-    autoUpdater.quitAndInstall();
-  });
+    autoUpdater.on('update-available', () => {
+      console.log('Update beschikbaar');
+    });
+
+    autoUpdater.on('update-not-available', () => {
+      console.log('Geen updates beschikbaar');
+    });
+
+    autoUpdater.on('error', (err) => {
+      console.error('Update fout:', err);
+    });
+
+    autoUpdater.on('update-downloaded', () => {
+      console.log('Update gedownload, herstart de app');
+      autoUpdater.quitAndInstall();
+    });
+  }
 });
